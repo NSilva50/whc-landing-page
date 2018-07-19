4fde200bbc2771d62b68191c6949d5e820f56caf
User <nathansilva50@gmail.com>
2018-07-19 16:55:36 -0700

    Frame done, next is more styling and finishing touches

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  menuItem: object = [
+    {
+      name: 'Download'
+    },
+    {
+      name: 'Features'
+    },
+    {
+      name: 'Contact'
+    }
+  ]
+
+  featureInfo: object = [
+    {
+      class: 'fas fa-mobile',
+      title: 'Device Mockups',
+      content: 'Ready to use HTML/CSS device mockups, no Photoshop required!'
+    },
+    {
+      class: 'fas fa-camera',
+      title: 'Flexible Use',
+      content: 'Put an image, video, animation, or anything else in the screen!'
+    },
+    {
+      class: 'fas fa-gift',
+      title: 'Free to Use',
+      content: 'As always, this theme is free to download and use for any purpose!'
+    },
+    {
+      class: 'fas fa-lock-open',
+      title: 'Open Source',
+      content: 'Since this theme is MIT licensed, you can use it commercially!'
+    }
+  ]
+
+  socialImg: object = [
+    {
+      class: 'fa fa-twitter'
+    },
+    {
+      class: 'fa fa-facebook'
+    },
+    {
+      class: 'fa fa-google-plus'
+    }
+  ]
 }
